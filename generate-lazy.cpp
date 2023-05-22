c905fb32b455f7bf6070d337f65a0d9fc7111fcc
oraqlle <41113853+oraqlle@users.noreply.github.com>
2023-05-22 18:22:22 +1000

    Added logging for generators

--- generate-lazy.cpp
+++ generate-lazy.cpp
@@ -8,6 +8,7 @@
 // by in some of the challenges.
 //
 // Build
+// $ module load gcc/10.2.0
 // $ g++ -std=c++20 -o bin/generate-lazy generate-lazy.cpp
 //
 // Run
@@ -27,7 +28,7 @@
 
 namespace fs = std::filesystem;
 
-using clock = std::chrono::high_resolution_clock;
+using clock_type = std::chrono::high_resolution_clock;
 
 constexpr std::size_t default_problem_size = 1'000'000uL;
 
@@ -66,30 +67,53 @@ namespace make {
 
 auto sum(std::size_t problem_size) -> void
 {
-    auto auto nums = std::views::iota(0)
+    auto start_gen = clock_type::now();
+
+    auto nums = std::views::iota(0)
         | generate
         | std::views::take(problem_size)
         | std::views::common;
 
     auto sum = std::accumulate(nums.begin(), nums.end(), double {});
+    auto end_gen = clock_type::now();
+    auto time_gen = std::chrono::duration_cast<std::chrono::seconds>(end_gen - start_gen).count();
+    std::clog << "Finished Generating [Sum]. Took: " << time_gen << "s" << std::endl;
 
+    auto start_write = clock_type::now();
     write(sum_path / "input.txt", nums);
     write(sum_path / "output.txt", sum);
+    auto end_write = clock_type::now();
+    auto time_write = std::chrono::duration_cast<std::chrono::seconds>(end_write - start_write).count();
+    std::clog << "Finished Writing [Sum]. Took: " << time_write << "s" << std::endl;
 }
 
 auto mergesort(std::size_t problem_size) -> void
 {
-    auto nums = std::views::iota(0) | generate | std::views::take(problem_size);
-    write(mergesort_path / "unsorted.txt", nums);
+    auto start_input_write = clock_type::now();
+    auto nums = std::views::iota(0) | generate | std::views::take(problem_size) | std::views::common;
+    auto unsorted = std::vector<double>(nums.begin(), nums.end());
+    write(mergesort_path / "unsorted.txt", unsorted);
+    auto end_input_write = clock_type::now();
+    auto time_input_write = std::chrono::duration_cast<std::chrono::seconds>(end_input_write - start_input_write).count();
+    std::clog << "Finished Writing [mergesort - input]. Took: " << time_input_write << "s" << std::endl;
 
-    std::ranges::sort(nums);
+    auto start_output_gen = clock_type::now();
+    std::ranges::sort(unsorted);
 
-    if (!std::ranges::is_sorted(nums)) {
+    if (!std::ranges::is_sorted(unsorted)) {
         std::clog << "Didn't sort input" << std::endl;
         return;
     }
 
-    write(mergesort_path / "sorted.txt", nums);
+    auto end_output_gen = clock_type::now();
+    auto time_output_gen = std::chrono::duration_cast<std::chrono::seconds>(end_output_gen - start_output_gen).count();
+    std::clog << "Finished Generating [mergesort - input]. Took: " << time_output_gen << "s" << std::endl;
+
+    auto start_output_write = clock_type::now();
+    write(mergesort_path / "sorted.txt", unsorted);
+    auto end_output_write = clock_type::now();
+    auto time_output_write = std::chrono::duration_cast<std::chrono::seconds>(end_output_write - start_output_write).count();
+    std::clog << "Finished Writing [mergesort - input]. Took: " << time_output_write << "s" << std::endl;
 }
 
 } // namespace make
@@ -103,6 +127,7 @@ int main(int argc, char* argv[])
         problem_size = std::ranges::clamp(problem_size, default_problem_size, std::vector<double> {}.max_size());
     }
 
+    std::clog << "MDN HPC Training Lazy Generator" << std::endl;
     make::sum(problem_size);
     make::mergesort(problem_size);
 
--- generate.cpp
+++ generate.cpp
@@ -8,23 +8,26 @@
 // by in some of the challenges.
 //
 // Build
+// $ module load gcc/10.2.0
 // $ g++ -std=c++20 -o bin/generate generate.cpp
 //
 // Run
-// $ bin/generate
+// $ bin/generate 1000000000
 
 #include <algorithm>
+#include <chrono>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <numeric>
 #include <random>
 #include <ranges>
-
 #include <string_view>
 #include <vector>
 
 namespace fs = std::filesystem;
+
+using clock_type = std::chrono::high_resolution_clock;
 
 constexpr std::size_t default_problem_size = 1'000'000uL;
 
@@ -64,27 +67,50 @@ namespace make {
 
 auto sum(std::vector<double>& buffer) -> void
 {
+    auto start_gen = clock_type::now();
     generate(buffer);
-
     auto sum = std::accumulate(buffer.cbegin(), buffer.cend(), double {});
+    auto end_gen = clock_type::now();
+    auto time_gen = std::chrono::duration_cast<std::chrono::seconds>(end_gen - start_gen).count();
+    std::clog << "Finished Generating [Sum]. Took: " << time_gen << "s" << std::endl;
 
+    auto start_write = clock_type::now();
     write(sum_path / "input.txt", buffer);
     write(sum_path / "output.txt", sum);
+    auto end_write = clock_type::now();
+    auto time_write = std::chrono::duration_cast<std::chrono::seconds>(end_write - start_write).count();
+    std::clog << "Finished Writing [Sum]. Took: " << time_write << "s" << std::endl;
 }
 
 auto mergesort(std::vector<double>& buffer) -> void
 {
+    auto start_input_gen = clock_type::now();
     generate(buffer);
+    auto end_input_gen = clock_type::now();
+    auto time_input_gen = std::chrono::duration_cast<std::chrono::seconds>(end_input_gen - start_input_gen).count();
+    std::clog << "Finished Generating [mergesort - input]. Took: " << time_input_gen << "s" << std::endl;
+
+    auto start_input_write = clock_type::now();
     write(mergesort_path / "unsorted.txt", buffer);
+    auto end_input_write = clock_type::now();
+    auto time_input_write = std::chrono::duration_cast<std::chrono::seconds>(end_input_write - start_input_write).count();
+    std::clog << "Finished Writing [mergesort - input]. Took: " << time_input_write << "s" << std::endl;
 
+    auto start_output_gen = clock_type::now();
     std::ranges::sort(buffer);
-
     if (!std::ranges::is_sorted(buffer)) {
-        std::clog << "Didn't sort input" << std::endl;
+        std::clog << "Didn't sort input properly" << std::endl;
         return;
     }
+    auto end_output_gen = clock_type::now();
+    auto time_output_gen = std::chrono::duration_cast<std::chrono::seconds>(end_output_gen - start_output_gen).count();
+    std::clog << "Finished Generating [mergesort - input]. Took: " << time_output_gen << "s" << std::endl;
 
+    auto start_output_write = clock_type::now();
     write(mergesort_path / "sorted.txt", buffer);
+    auto end_output_write = clock_type::now();
+    auto time_output_write = std::chrono::duration_cast<std::chrono::seconds>(end_output_write - start_output_write).count();
+    std::clog << "Finished Writing [mergesort - input]. Took: " << time_output_write << "s" << std::endl;
 }
 
 } // namespace make
@@ -99,7 +125,7 @@ int main(int argc, char* argv[])
     }
 
     auto buffer = std::vector<double>(problem_size, 0.0);
-
+    std::clog << "MDN HPC Training Generator" << std::endl;
     make::sum(buffer);
     make::mergesort(buffer);
 
